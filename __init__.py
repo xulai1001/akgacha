aad490fa5b1f37f014eb469aa353b658c0d7debf
viktorxu <xulai1001@gmail.com>
2021-01-23 09:03:11 +0800

    working demo

--- __init__.py
+++ __init__.py
@@ -8,9 +8,10 @@ from hoshino import Service, priv, util
 from hoshino.typing import *
 from .akgacha import Gacha
 
+working_path = "hoshino/modules/akgacha/"
 img_path = "./images"
-char_data = json.load(open("character_table.json", encoding="utf-8"))
-gacha_data = json.load(open("config.json", encoding="utf-8"))
+char_data = json.load(open(working_path + "character_table.json", encoding="utf-8"))
+gacha_data = json.load(open(working_path + "config.json", encoding="utf-8"))
 
 sv_help = '''
 [@Bot方舟十连] 明日方舟抽卡
@@ -18,26 +19,40 @@ sv_help = '''
 [查看方舟卡池] 当前卡池信息
 [切换方舟卡池] 更改卡池
 '''.strip()
-sv = Service('akgacha', help=sv_help)
+sv = Service('akgacha', help_=sv_help, enable_on_default=True)
 
-group_pool = {}
+group_banner = {}
 try:
-    group_pool = json.load(open("group_pool.json", encoding="utf-8"))
+    group_banner = json.load(open("group_banner.json", encoding="utf-8"))
 except FileNotFoundError: pass
     
 def save_group_pool():
-    with open("group_pool.json", encoding="utf-8") as f:
-        json.dump(group_pool, f, ensure_ascii=False)
+    with open("group_banner.json", encoding="utf-8") as f:
+        json.dump(group_banner, f, ensure_ascii=False)
         
+@sv.on_fullmatch(("查看方舟卡池"))
 async def gacha_info(bot, ev: CQEvent):
-    await bot.send(ev, "查看卡池命令")
-    
+    gid = str(ev.group_id)
+    if not gid in group_banner:
+        group_banner[gid] = "普池"
+    banner = group_banner[gid]
+    gacha = Gacha()
+    gacha.set_banner(banner)
+    line = [banner]
+    line.append(f"up角色: {' '.join(gacha.banner['up_6']+gacha.banner['up_5']+gacha.banner['up_4'])}")
+    line.append(f"限定池: {'是' if gacha.banner['limited'] else '否'}")
+    print(line)
+    await bot.send(ev, '\n'.join(line))
+
+@sv.on_prefix(("切换方舟卡池"))
 async def set_pool(bot, ev: CQEvent):
     await bot.send(ev, "设置卡池命令")
-    
+
+@sv.on_prefix(("方舟十连"), only_to_me=True)
 async def gacha_10(bot, ev: CQEvent):
     await bot.send(ev, "10连", at_sender=True)
 
+@sv.on_prefix(("方舟来一井"), only_to_me=True)
 async def gacha_300(bot, ev: CQEvent):
     await bot.send(ev, "抽一井", at_sender=True)
     
